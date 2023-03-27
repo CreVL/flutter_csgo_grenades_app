554cd22fb2b2091e04de9e9dddc954153f39c5ce
CreVL <91658458+CreVL@users.noreply.github.com>
2023-03-27 11:10:30 +0300

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_csgo_grenades_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_csgo_grenades_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+          primarySwatch: Colors.yellow, scaffoldBackgroundColor: Colors.black),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Maps'),
+      ),
+    );
+  }
+}
