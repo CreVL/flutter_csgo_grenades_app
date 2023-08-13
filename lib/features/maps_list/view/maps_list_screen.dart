016759bdaf6dca9c572fa284a664edee752ac045
CreVL <91658458+CreVL@users.noreply.github.com>
2023-08-13 13:17:51 +0300

    Changes the size of map widgets

--- lib/features/maps_list/view/maps_list_screen.dart
+++ lib/features/maps_list/view/maps_list_screen.dart
@@ -34,59 +34,65 @@ class _MapsListScreenState extends State<MapsListScreen> {
         bloc: _mapListBloc,
         builder: (context, state) {
           if (state is MapListLoaded) {
-            return Column(
-              children: List.generate(state.mapsList.length, (index) {
-                final map = state.mapsList[index];
-                return Expanded(
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (_) => MapsGrenadesScreen(
-                            title: map.title,
-                            mapImage: map.mapImage,
-                            mapId: map.mapId,
+            return LayoutBuilder(
+              builder: (context, constraints) {
+                return ListView.builder(
+                  itemCount: state.mapsList.length,
+                  itemBuilder: (context, index) {
+                    final map = state.mapsList[index];
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => MapsGrenadesScreen(
+                              title: map.title,
+                              mapImage: map.mapImage,
+                              mapId: map.mapId,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.4,
+                        height: constraints.maxWidth * 0.4,
+                        margin: const EdgeInsets.all(10),
+                        decoration: const BoxDecoration(
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black,
+                              spreadRadius: 5,
+                              blurRadius: 10,
+                              offset: Offset(3, 3),
+                            ),
+                          ],
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          gradient: LinearGradient(
+                            colors: [
+                              Color(0xFFFFD100),
+                              Color(0xFFFFFFFF),
+                              Color(0xFFFFD100),
+                            ],
                           ),
                         ),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.all(10),
-                      decoration: const BoxDecoration(
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black,
-                            spreadRadius: 5,
-                            blurRadius: 10,
-                            offset: Offset(3, 3),
-                          ),
-                        ],
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(10),
-                        ),
-                        gradient: LinearGradient(
-                          colors: [
-                            Color(0xFFFFD100),
-                            Color(0xFFFFFFFF),
-                            Color(0xFFFFD100),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image(
+                              image: map.mapLogo.image,
+                              fit: BoxFit.contain,
+                            ),
                           ],
                         ),
                       ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image(
-                            image: map.mapLogo.image,
-                            fit: BoxFit.contain,
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
+                    );
+                  },
                 );
-              }),
+              },
             );
+
           }
           if (state is MapListLoadingFailed) {
             return Center(
